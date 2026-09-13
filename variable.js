03fea9ba20262c268f0fb81d9b103755aa73ffcf
shifa0722 <pshifa304@gmail.com>
2026-09-13 23:53:46 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,53 @@
+const product ={
+    pname : "parker jotter",
+    pRating : 5,
+    offer : 10 ,
+    pprice : 270,
+
+}
+
+console.log(product);
+
+
+assignment operators
+ a= 10;
+b =20;
+
+a+= 4;
+console.log(a);
+
+
+Comparison operators
+
+let a= 10;
+let b= 50;
+
+a==b;
+console.log("a==b",a==b);
+
+// logical operators
+
+let x= 3;
+let y =5;
+
+let  cond1= x!=y;
+let cond2 = x< y;
+
+console.log ("result", cond1 && cond2);
+
+
+// conditional statements
+
+let age =25;
+
+if (age >= 18) {
+    console.log("you can vote");
+}
+
+if (age < 18){
+    console.log("you cannot vote");
+}
+
+
+
+
