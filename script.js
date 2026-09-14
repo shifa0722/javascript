8df58eedb87defc234a452255f19ce5911bf4e77
shifa0722 <pshifa304@gmail.com>
2026-09-14 23:36:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// let a=8 
+
+// if (a%2==0){
+//     console.log("even");
+
+// } else {
+//     console.log("odd");
+
+// }
+
+
+// let mode = "dark";
+
+// color ;
+
+// if (mode==="dark"){
+//     color= "black";
+
+// } else if (mode==="pink"){
+//     console.log("pink");
+
+// } else {
+//     console.log("white");
+// }
+
+
+// ternary operators
+
+let age = 23;
+
+ let result = age > 18 ? "can vote" : "cannot vote";
+ console.log(result);
+
+ 
