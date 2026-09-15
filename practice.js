4c73c8c4a367bba81383f7e38ab72a7e1c4f1458
shifa0722 <pshifa304@gmail.com>
2026-09-15 23:39:12 +0530

    Add files via upload

--- practice.js
+++ practice.js
@@ -1,2 +1,37 @@
-let name = prompt("enter your name");
-console.log(name);
+// let name = prompt("enter your name");
+// console.log(name);
+
+
+// let sum =0;
+// for (let i=1 ; i%2==0; i++){
+//     sum += i;
+// }
+// console.log();
+
+
+
+// print all even numbers from 1 to 100
+
+// for( let i=2; i<=100 ; i+=2){
+//     console.log(i);
+// }
+
+
+// 2nd way to print using module
+
+// for(i=1 ;i<=100 ;i++){
+//     if(i% 2==0){
+//         console.log(i);
+//     }
+// }
+
+
+
+// 3rd way using while loop
+
+let i=1;
+while(i<=20){
+    if(i% 2==0){
+    console.log(i);
+}
+}
